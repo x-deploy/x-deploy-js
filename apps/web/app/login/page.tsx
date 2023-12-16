be19f5e060ce0844e3c8f53029dec7430bbc1089
bastosbobSchool <72014566+bastosbob@users.noreply.github.com>
2023-12-15 23:55:03 -0500

    add: better padding

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -1,37 +1,39 @@
-import {Card, CardContent,CardTitle} from "../../components/basics/card";
+import {Card, CardContent, CardTitle} from "../../components/basics/card";
 import {Button} from "../../components/basics/buttons";
 import {TextInput} from "../../components/basics/text-input";
 
 
 export default function Page(): JSX.Element {
-  return (
-     <div style={
-         {
-            width: "100%",
-             height: "100vh",
-             paddingLeft: "10px",
-             display: "flex",
-             justifyContent: "center",
-             alignItems: "center"
-         }
-     }>
-         <Card style={
+    return (
+        <div style={
+            {
+                width: "100%",
+                height: "100vh",
+                paddingLeft: "10px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }
+        }>
+            <Card style={
                 {
                     width: "400px",
                     height: "400px"
                 }
-         }>
-             <CardContent>
-                <CardTitle className="text-center">
-                    Welcome
-                </CardTitle>
-                 <TextInput label={"Username"} placeholder={"Enter your username"}/>
-                    <TextInput label={"Password"} placeholder={"Enter your password"}/>
-                    <Button variant={"primary"} className="mt-4">
-                        Login
-                    </Button>
-            </CardContent>
-         </Card>
-     </div>
-  );
+            }>
+                <CardContent>
+                    <CardTitle className="text-center">
+                        Welcome
+                    </CardTitle>
+                    <div>
+                        <TextInput label={"Username"} placeholder={"Enter your username"}/>
+                        <TextInput label={"Password"} placeholder={"Enter your password"}/>
+                    </div>
+                    <div className={"my-3.5"}>
+                    <Button variant={"primary"} className="mt-4"> Login</Button>
+                    </div>
+                </CardContent>
+            </Card>
+        </div>
+    );
 }
