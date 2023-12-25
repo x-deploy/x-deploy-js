ecf00e0ac714d7b021cbb73927bdbc5685a1fbe4
bastosbobSchool <72014566+bastosbob@users.noreply.github.com>
2023-12-25 18:52:26 +0100

    fix: redirecting on index

--- apps/web/app/lib/action.ts
+++ apps/web/app/lib/action.ts
@@ -1,8 +1,6 @@
 "use server"
 import {redirect} from "next/navigation";
 export const login = async (formData : FormData) => {
-
-    try {
         const email = formData.get('email')
         const password = formData.get('password')
         const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/auth/login', {
@@ -13,13 +11,9 @@ export const login = async (formData : FormData) => {
             body: JSON.stringify({email, password}),
         })
         const data = await response.json()
-        //if data ok redirect to dashboard else show error
         if (response.ok) {
             redirect('/')
         } else {
             return data
         }
-    } catch (error) {
-        return {error : "Error"}
-    }
 }
