4325dfc70d029304686804d682ba74e3046345d2
bastosbobSchool <72014566+bastosbob@users.noreply.github.com>
2024-01-11 16:50:00 -0500

    add: basic popover

--- apps/web/components/navbar/avatar-menu.tsx
+++ apps/web/components/navbar/avatar-menu.tsx
@@ -1,11 +1,29 @@
 import {Avatar} from "../basics/avatar";
+import {Popover, PopoverContent, PopoverTrigger} from "../../@/components/ui/popover";
+import {Card, CardContent} from "../basics/card";
+import {Button} from "../basics/buttons";
 
-export const AvatarMenu = ({img} : {
+export const AvatarMenu = ({img}: {
     img: any;
 }) => {
     return (
         <div className={"hover:cursor-pointer"}>
-            <Avatar src={img}/>
+            <Popover>
+                <PopoverTrigger>
+                    <Avatar src={img}/>
+                </PopoverTrigger>
+                <PopoverContent style={
+                    {
+                        border: "none"
+                    }
+                }>
+                    <Card className={"bg-black"}>
+                        <CardContent>
+                            <Button variant={"secondary"}><span className={"text-red-500"}>Log Out</span></Button>
+                        </CardContent>
+                    </Card>
+                </PopoverContent>
+            </Popover>
         </div>
     )
 }
