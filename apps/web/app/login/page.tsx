43cfc58a4043b919f7a4bfedda2de35d1b1c4a2e
bastosbobSchool <72014566+bastosbob@users.noreply.github.com>
2024-02-12 15:17:20 -0500

    add: remove image at login

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -6,13 +6,13 @@ export default async function Page(): Promise<JSX.Element> {
     return (
         <div className="w-full h-screen flex justify-center items-center">
                 <div className="flex w-full h-full justify-between items-center">
-                    <div className="hidden lg:flex w-1/2 h-full justify-center items-center relative">
-                        <Image
-                            src={canada}
-                            alt="login-image"
-                            layout="fill"
-                            objectFit="cover"
-                        />
+                    <div className="hidden lg:flex w-1/2 h-full justify-center items-center relative bg-gray-800">
+                        {/*<Image*/}
+                        {/*    src={canada}*/}
+                        {/*    alt="login-image"*/}
+                        {/*    layout="fill"*/}
+                        {/*    objectFit="cover"*/}
+                        {/*/>*/}
                     </div>
                     <div className="flex w-full lg:w-1/2  h-full justify-center items-center">
                         <LoginForm/>
